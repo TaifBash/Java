c99d4124a40f7be6c5d301678f13bc47f42851df
Taif Basheikh <107805330+TaifBash@users.noreply.github.com>
2022-07-22 04:05:58 +0300

    Update YourName.java

--- YourName.java
+++ YourName.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package yourname;
-//Taif Basheikh 2005890
   import java.util.Scanner; // To take the name from user. 
+
 public class YourName {
     public static void main(String[] args) {
  // TODO code application logic here
