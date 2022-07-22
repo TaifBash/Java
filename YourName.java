7d5addf6bc1571048693bfc0b654c9e972a1e4ff
Taif Basheikh <107805330+TaifBash@users.noreply.github.com>
2022-07-22 04:04:58 +0300

    Add files via upload

---
+++ YourName.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yourname;
+//Taif Basheikh 2005890
+  import java.util.Scanner; // To take the name from user. 
+public class YourName {
+    public static void main(String[] args) {
+ // TODO code application logic here
+     Scanner input = new Scanner(System.in);
+String FirstName,LastName, FullName; 
+    
+            System.out.print("Enter your fires name: "); // Ask user Eneter first name
+        FirstName = input.nextLine();  // store first name at n1.
+    
+            System.out.print("Enter your last name: ");// Ask user Eneter last name
+        LastName = input.nextLine(); // store last name at n1.
+        
+    FullName=FirstName+" "+LastName;
+           System.out.println(FullName); }}//display
