1796078235255f02cee539fbd2e64ce6c595cabb
Taif Basheikh <107805330+TaifBash@users.noreply.github.com>
2022-07-24 14:20:23 +0300

    Update Myinfo.java

--- Myinfo.java
+++ Myinfo.java
@@ -1,25 +1,8 @@
-/*
- * Taif Basheikh
- * ID: 2005890
- */
-package myinfo;
-
-/**
- *
- * @author Windows10
- */
 public class Myinfo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
     String name="Taif";
     int age=21;
     double annualpay=100000.0;
-   
     System.out.println("My name is "+name+", my age is "+age+" and I hope to earn $"+annualpay+"per year.");
-
     }
-    
 }
